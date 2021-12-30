459561bb36421dc625a6ac84127d164f4119e4b2
kuafuwang <kuafuwang@qq.com>
2021-12-30 18:12:37 +0800

    Fixed error when getFileRawContent on win32

--- src/IcuUtil.cpp
+++ src/IcuUtil.cpp
@@ -81,9 +81,9 @@ namespace IcuUtil
     {
 
 #ifdef _MSC_VER
-        std::ifstream stream(IcuUtil::s2ws(fileName), std::ios::binary);
+        std::ifstream stream(fileName, std::ios::binary);
 #else
-        std::ifstream stream(ws2s(fileName), std::ios::binary);
+        std::ifstream stream(fileName, std::ios::binary);
 
 #endif
         if (!stream.good() || stream.eof()) // No fail, bad or EOF.
